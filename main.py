5b349f54c530ee027b671029654629c1498614d7
mboyd020 <147006222+mboyd020@users.noreply.github.com>
2025-04-04 12:40:28 -0700

    Update main.py
    Added guess fnc + test

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import hashlib
 fullHash = "team5:$1$zLGo2l86$srZPtyJQb2sQIaMdtHosV1:16653:0:99999:7:::"
 salt = "zLGo2l86"
 hash = "srZPtyJQb2sQIaMdtHosV1"
+def guess(guess):
+  if (hashlib.md5(guess + salt).hexdigest() == testhash):
+    print(guess)
+    return true
+  return false
+testhash = hashlib.md5("abc" + salt).hexdigest()
+guess("abc")
