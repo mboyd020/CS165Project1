be2ea9ef6c77519165305af4d31f766f2dc8ee6c
mboyd020 <mboyd020@ucr.edu>
2025-04-04 17:17:18 -0700

    added progress meter to dict generator to make it more clear

--- generate_dict.py
+++ generate_dict.py
@@ -1,5 +1,6 @@
 #create a file that contains all lowercase passwords from a-z of length 6 or less
 password_file = open("passwords.txt", "w")
+count = 0
 for i in range(1, 7):
     print("Generating passwords of length " + str(i) + "...")
     for j in range(0, 26**i):
@@ -9,5 +10,8 @@ for i in range(1, 7):
             password += chr(num % 26 + 97)
             num //= 26
         password_file.write(password + "\n")
+        count += 1
+        if (count % 1000000) == 0:
+            print(f"{count/321272406}% done")
 print("done")
 password_file.close()
